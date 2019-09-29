4a7bca4d148b7fd91a379b6399486d51afd6a747
Alexander Shemanov <demonphizik@gmail.com>
2019-09-29 18:04:25 +0300

    Добавьте файлы проекта.

---
+++ MyFirstWorkflow/PortListenerActvity.cs
@@ -0,0 +1,113 @@
+﻿using NLog;
+using System;
+using System.Activities;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace MyFirstWorkflow
+{
+    public class PortListenerActvity : CodeActivity
+    {
+        private Logger _logger = LogManager.GetCurrentClassLogger();
+        private AutoResetEvent _exitSignal = new AutoResetEvent(true);
+
+        [RequiredArgument]
+        public InArgument<int> Port { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            try
+            {
+                Start(Port.Get(context));
+            }
+            catch (SocketException exp)
+            {
+                _logger.Error("SocketException: {0}", exp);
+                Stop();
+            }
+            _exitSignal.WaitOne();
+        }
+
+        public void Stop()
+        {
+            _exitSignal.Set();
+        }
+
+        private void Start(int port)
+        {
+            _exitSignal.Reset();
+
+            var IPv4Addresses = Dns.GetHostEntry(Dns.GetHostName())
+              .AddressList.Where(al => al.AddressFamily == AddressFamily.InterNetwork)
+              .ToList();
+
+            foreach (IPAddress ip in IPv4Addresses)
+            {
+                Sniff(ip, port);
+            }
+        }
+
+        private void Sniff(IPAddress ip, int port, int startDataPos = 40)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.IP);
+
+            socket.Bind(new IPEndPoint(ip, 0));
+            socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.HeaderIncluded, true);
+            socket.IOControl(IOControlCode.ReceiveAll, new byte[4] { 1, 0, 0, 0 }, null);
+
+            byte[] buffer = new byte[65548];
+            //socket.Receive(buffer, 0, buffer.Length, SocketFlags.None);
+
+            //data recieving and processing
+            void OnReceive(IAsyncResult ar)
+            {
+                var count = socket.EndReceive(ar);
+                var protocol = ToProtocolString(buffer[9]);
+                var fromIP = new IPAddress(BitConverter.ToUInt32(buffer, 12));
+                var toIP = new IPAddress(BitConverter.ToUInt32(buffer, 16));
+                var fromPort = (ushort)IPAddress.NetworkToHostOrder(BitConverter.ToInt16(buffer, 20));
+                var toPort = (ushort)IPAddress.NetworkToHostOrder(BitConverter.ToInt16(buffer, 22));
+                var totalLength = (ushort)IPAddress.NetworkToHostOrder(BitConverter.ToInt16(buffer, 2));
+
+                if (toPort == port || fromPort == port)
+                {
+                    StringBuilder strBulder = new StringBuilder();
+                    strBulder.AppendFormat("{0}\t{1}:{2}\t===>\t{3}:{4}", protocol, fromIP, fromPort, toIP, toPort);
+                    if (totalLength > startDataPos)
+                    {
+                        var strBytes = buffer.Skip(startDataPos).Take(totalLength - startDataPos).Select(b => (byte)(b < 32 ? 32 : b)).ToArray();
+                        var str = Encoding.UTF8.GetString(strBytes);
+                        strBulder.AppendLine(str);
+                        _logger.Info(strBulder.ToString());
+                    }
+                }
+                //clean out our buffer
+                Array.Clear(buffer, 0, buffer.Length);
+                //continue receiving
+                socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(OnReceive), null);
+            }
+
+            //begin listening to the socket
+            socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(OnReceive), null);
+        }
+
+        private string ToProtocolString(byte b)
+        {
+            switch (b)
+            {
+                case 1: return "ICMP";
+                case 2: return "IGMP";
+                case 6: return "TCP";
+                case 17: return "UDP";
+                case 41: return "ENCAP";
+                case 89: return "OSPF";
+                case 132: return "SCTP";
+                default: return "#" + b.ToString();
+            }
+        }
+
+    }
+}
---
+++ MyFirstWorkflow/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Activities;
+
+namespace MyFirstWorkflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Activity workflow = new PortListenerActvity() { Port = 80 };
+
+            Console.WriteLine("Прослушивание порта...");
+            try
+            {
+                WorkflowInvoker.Invoke(workflow);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp);
+            }
+            Console.WriteLine("Прослушивание порта окончено");
+
+
+            Console.WriteLine("Нажмите клавишу для выхода");
+            Console.ReadLine();
+
+            //    AutoResetEvent syncEvent = new AutoResetEvent(false);
+            //    AutoResetEvent idleEvent = new AutoResetEvent(false);
+
+            //    var inputs = new Dictionary<string, object>() { { "MaxNumber", 100 } };
+
+            //    WorkflowApplication wfApp =
+            //        new WorkflowApplication(new Game(), inputs);
+
+            //    wfApp.Completed = delegate (WorkflowApplicationCompletedEventArgs e)
+            //    {
+            //        int Turns = Convert.ToInt32(e.Outputs["Turns"]);
+            //        Console.WriteLine("Congratulations, you guessed the number in {0} turns.", Turns);
+
+            //        syncEvent.Set();
+            //    };
+
+            //    wfApp.Aborted = delegate (WorkflowApplicationAbortedEventArgs e)
+            //    {
+            //        Console.WriteLine(e.Reason);
+            //        syncEvent.Set();
+            //    };
+
+            //    wfApp.OnUnhandledException = delegate (WorkflowApplicationUnhandledExceptionEventArgs e)
+            //    {
+            //        Console.WriteLine(e.UnhandledException.ToString());
+            //        return UnhandledExceptionAction.Terminate;
+            //    };
+
+            //    wfApp.Idle = delegate (WorkflowApplicationIdleEventArgs e)
+            //    {
+            //        idleEvent.Set();
+            //    };
+
+            //    wfApp.Run();
+
+            //    // Loop until the workflow completes.
+            //    WaitHandle[] handles = new WaitHandle[] { syncEvent, idleEvent };
+            //    while (WaitHandle.WaitAny(handles) != 0)
+            //    {
+            //        // Gather the user input and resume the bookmark.
+            //        bool validEntry = false;
+            //        while (!validEntry)
+            //        {
+            //            if (!Int32.TryParse(Console.ReadLine(), out int Guess))
+            //            {
+            //                Console.WriteLine("Please enter an integer.");
+            //            }
+            //            else
+            //            {
+            //                validEntry = true;
+            //                wfApp.ResumeBookmark("EnterGuess", Guess);
+            //            }
+            //        }
+            //    }
+            //    Console.ReadLine();
+        }
+
+    }
+}
